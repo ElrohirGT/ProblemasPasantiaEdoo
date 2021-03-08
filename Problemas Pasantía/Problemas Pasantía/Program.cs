219a05b49e1a08581883651c8d7223b619f0b2fa
ElrohirGT <elrohirgt@gmail.com>
2021-03-08 14:54:12 -0600

    Created the project template.

---
+++ Problemas Pasantía/Problemas Pasantía/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Problemas_Pasantía
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
